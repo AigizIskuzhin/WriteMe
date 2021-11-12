38d7e414c858b7e73441a52dfe2b7b09ddb37bd3
Adjax <aigiz.iskuzhin@yandex.ru>
2021-11-12 17:33:53 +0500

    тесты

--- Website.Tests/AuthenticateControllerTests.cs
+++ Website.Tests/AuthenticateControllerTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Website.Controllers;
 using Website.Infrastructure.Services.Interfaces;
@@ -10,11 +11,6 @@ namespace Website.Tests
     public class AuthenticateControllerTests
     {
         #region ConfirmMailForAuthorization
-
-        #region ConfirmMailForAuthorization with empty viewModel
-        /// <summary>
-        /// HttpGet ConfirmMailForAuthorization with empty viewModel
-        /// </summary>
         [Fact]
         public void ConfirmMailForAuthorization_returns_ViewResult()
         {
@@ -29,33 +25,24 @@ namespace Website.Tests
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result.Result);
             var model = Assert.IsAssignableFrom<ConfirmMailViewModel>(viewResult.ViewData.Model);
-            Assert.Equal(viewModel, model);
-
-            // Expected to have an error on required field
             var modelState = viewResult.ViewData.ModelState;
-            Assert.False(modelState.IsValid);
-
             var errorsCount = viewResult.ViewData.ModelState.ErrorCount;
+            
+            Assert.Equal(viewModel, model);
+            // Expected to have an error on required field
+            Assert.False(modelState.IsValid);
             Assert.Equal(1, errorsCount);
-
-            //var fieldName = "MailAddress";
-            //var modelStateEntry = viewResult.ViewData.ModelState[fieldName];
         }
-        #endregion
-
-        #region ConfirmMailForAuthorization with nonvalid mail address
-
-        /// <summary>
-        /// HttpPost ConfirmMailForAuthorization with nonvalid mail address
-        /// </summary>
         [Fact]  
-        public void ConfirmMailForAuthorization_withNonValidMailAddress_returns_ViewResult_errorWithNonValidMailAddress()
+        public void ConfirmMailForAuthorization_withMail_test_returns_error()
         {
             // Arrange
             var mockService = new Mock<IAuthenticateService>();
             var controller = new AuthenticateController(mockService.Object);
-            var viewModel = new ConfirmMailViewModel();
-            viewModel.MailAddress = "test";
+            var viewModel = new ConfirmMailViewModel
+            {
+                MailAddress = "test"
+            };
 
             // Act
             var result = controller.ConfirmMailForAuthorization(viewModel);
@@ -63,29 +50,17 @@ namespace Website.Tests
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result.Result);
             var model = Assert.IsAssignableFrom<ConfirmMailViewModel>(viewResult.ViewData.Model);
-            Assert.Equal(viewModel, model);
-
             var modelState = viewResult.ViewData.ModelState;
-
+            Assert.Equal(viewModel, model);
             Assert.False(modelState.IsValid);
         }
-        #endregion
-
-        #region ConfirmMailForAuthorization with right mailAddress format and get error with non existent user
-
-        /// <summary>
-        /// HttpPost ConfirmMailForAuthorization with right mailAddress got error with non existent user
-        /// </summary>
-        [Fact]
-        public void ConfirmMailForAuthorization_with_rightMailAddress_returns_ViewResult_with_errorNonExistentUser()
+        [Fact]  
+        public void ConfirmMailForAuthorization_withWhiteSpaceMail_returns_error()
         {
             // Arrange
             var mockService = new Mock<IAuthenticateService>();
             var controller = new AuthenticateController(mockService.Object);
             var viewModel = new ConfirmMailViewModel();
-            viewModel.MailAddress = "test@mail.ru";
-
-            mockService.Setup(s => s.IsUserExistAsync(viewModel.MailAddress)).Returns(() => null);
 
             // Act
             var result = controller.ConfirmMailForAuthorization(viewModel);
@@ -93,22 +68,13 @@ namespace Website.Tests
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result.Result);
             var model = Assert.IsAssignableFrom<ConfirmMailViewModel>(viewResult.ViewData.Model);
-            Assert.Equal(viewModel, model);
-
             var modelState = viewResult.ViewData.ModelState;
-
+            Assert.Equal(viewModel, model);
             Assert.False(modelState.IsValid);
+            //Assert.Equal("Неверный формат почты", modelState.Values.First().Errors.First().ErrorMessage);
         }
-
-        #endregion
-
-        #region ConfirmMailForAuthorization with right mailAddress format and get redirect to password input site
-
-        /// <summary>
-        /// HttpPost ConfirmMailForAuthorization with right mailAddress got error with non existent user
-        /// </summary>
         [Fact]
-        public void ConfirmMailForAuthorization_with_rightMailAddress_returns_ViewResult_for_passwordConfirm()
+        public void ConfirmMailForAuthorization_withRightMailFormat_returns_errorNonExistentUser()
         {
             // Arrange
             var mockService = new Mock<IAuthenticateService>();
@@ -117,10 +83,31 @@ namespace Website.Tests
             {
                 MailAddress = "test@mail.ru"
             };
-            //mockService.Setup(s => s.IsUserExistAsync(viewModel.MailAddress)).Returns(() =>
-            //{
-            //    return new Task<User>(() => new User());
-            //});
+
+            mockService.Setup(s => s.IsUserExistAsync(viewModel.MailAddress)).ReturnsAsync(()=>null);
+
+            // Act
+            var result = controller.ConfirmMailForAuthorization(viewModel);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result.Result);
+            var model = Assert.IsAssignableFrom<ConfirmMailViewModel>(viewResult.ViewData.Model);
+            var modelState = viewResult.ViewData.ModelState;
+            Assert.Equal(viewModel, model);
+            Assert.False(modelState.IsValid);
+            Assert.Equal("Неверный адрес",modelState.Values.First().Errors.First().ErrorMessage);
+        }
+        [Fact]
+        public void ConfirmMailForAuthorization_with_rightMail_returns_ViewResult_for_passwordConfirm()
+        {
+            // Arrange
+            var mockService = new Mock<IAuthenticateService>();
+            var controller = new AuthenticateController(mockService.Object);
+            var viewModel = new ConfirmMailViewModel
+            {
+                MailAddress = "test@mail.ru"
+            };
+            mockService.Setup(s => s.IsUserExistAsync(viewModel.MailAddress)).ReturnsAsync(() => new());
 
             // Act
             var result = controller.ConfirmMailForAuthorization(viewModel);
@@ -128,15 +115,56 @@ namespace Website.Tests
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result.Result);
             var model = Assert.IsAssignableFrom<AuthorizationViewModel>(viewResult.ViewData.Model);
-            Assert.Equal(viewModel, model);
+            var modelState = viewResult.ViewData.ModelState;
+            Assert.IsType<AuthorizationViewModel>(model);
+            Assert.True(modelState.IsValid);
+            Assert.Equal("EnterPassword",viewResult.ViewName);
+        }
+        #endregion
+        
+        #region Enter password for authorization
 
+        [Fact]
+        public void EnterPassword_with_noMail_returns_ConfirmMailForAuthorization()
+        {
+            // Arrange
+            var mockService = new Mock<IAuthenticateService>();
+            var controller = new AuthenticateController(mockService.Object);
+            var viewModel = new AuthorizationViewModel();
+
+            // Act
+            var result = controller.EnterPassword(viewModel);
+
+            // Assert
+            var redirect = Assert.IsType<RedirectToActionResult>(result.Result);
+            
+            Assert.Equal(nameof(controller.ConfirmMailForAuthorization),redirect.ActionName);
+        }
+        [Fact]
+        public void EnterPassword_with_wrongPassword_returns_errorWithPassword()
+        {
+            // Arrange
+            var mockService = new Mock<IAuthenticateService>();
+            var controller = new AuthenticateController(mockService.Object);
+            var viewModel = new AuthorizationViewModel
+            {
+                MailAddress = "test@mail.ru",
+                Password = "1234"
+            };
+            mockService.Setup(s => s.ConfirmUserAsync(viewModel.MailAddress, viewModel.Password))
+                .ReturnsAsync(() => null);
+
+            // Act
+            var result = controller.EnterPassword(viewModel);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result.Result);
+            var model = Assert.IsAssignableFrom<AuthorizationViewModel>(viewResult.ViewData.Model);
             var modelState = viewResult.ViewData.ModelState;
 
-            Assert.False(modelState.IsValid);
+            Assert.IsType<AuthorizationViewModel>(viewResult.Model);
+            Assert.Equal("Неверный пароль", modelState.Values.First().Errors.First().ErrorMessage);
         }
-
-        #endregion
-
         #endregion
 
         #region ConfirmMailForRegistration
@@ -159,58 +187,97 @@ namespace Website.Tests
             var errorsCount = viewResult.ViewData.ModelState.ErrorCount;
             Assert.Equal(1, errorsCount);
         }
-
-        
-        #region ConfirmMailForAuthorization with nonvalid mail address
-
-        /// <summary>
-        /// ConfirmMailForAuthorization with nonvalid mail address
-        /// </summary>
         [Fact]  
-        public void ConfirmMailForRegistration_withNonValidMailAddress_returns_ViewResult_errorWithNonValidMailAddress()
+        public void ConfirmMailForRegistration_withMail_test_returns_error()
+        {
+            // Arrange
+            var mockService = new Mock<IAuthenticateService>();
+            var controller = new AuthenticateController(mockService.Object);
+            var viewModel = new ConfirmMailViewModel
+            {
+                MailAddress = "test"
+            };
+
+            // Act
+            var result = controller.ConfirmMailForRegistration(viewModel);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result.Result);
+            var model = Assert.IsAssignableFrom<ConfirmMailViewModel>(viewResult.ViewData.Model);
+            var modelState = viewResult.ViewData.ModelState;
+            Assert.Equal(viewModel, model);
+            Assert.False(modelState.IsValid);
+        }
+        [Fact]  
+        public void ConfirmMailForRegistrationn_withWhiteSpaceMail_returns_error()
         {
             // Arrange
             var mockService = new Mock<IAuthenticateService>();
             var controller = new AuthenticateController(mockService.Object);
             var viewModel = new ConfirmMailViewModel();
-            viewModel.MailAddress = "test";
 
             // Act
-            var result = controller.ConfirmMailForAuthorization(viewModel);
+            var result = controller.ConfirmMailForRegistration(viewModel);
 
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result.Result);
             var model = Assert.IsAssignableFrom<ConfirmMailViewModel>(viewResult.ViewData.Model);
-            Assert.Equal(viewModel, model);
-
             var modelState = viewResult.ViewData.ModelState;
-
+            Assert.Equal(viewModel, model);
             Assert.False(modelState.IsValid);
+            //TODO: FIX IT
+            //Assert.Equal("Неверный формат почты", modelState.Values.First().Errors.First().ErrorMessage);
         }
-        #endregion
-
-        #endregion
-
-        #region EnterPassword for authorization
-
         [Fact]
-        public void EnterPassword_with_noMail_returns_ConfirmMailForAuthorization()
+        public void ConfirmMailForRegistration_withRightMailFormat_returns_enterUserInfo()
         {
             // Arrange
             var mockService = new Mock<IAuthenticateService>();
             var controller = new AuthenticateController(mockService.Object);
-            var viewModel = new AuthorizationViewModel();
+            var viewModel = new ConfirmMailViewModel
+            {
+                MailAddress = "test@mail.ru"
+            };
+
+            mockService.Setup(s => s.IsUserExistAsync(viewModel.MailAddress)).ReturnsAsync(()=>null);
 
             // Act
-            var result = controller.EnterPassword(viewModel);
+            var result = controller.ConfirmMailForRegistration(viewModel);
 
             // Assert
             var viewResult = Assert.IsType<ViewResult>(result.Result);
-
-            Assert.IsType<ConfirmMailViewModel>(viewResult.Model);
-            Assert.True(viewResult.ViewName=="ConfirmMailForAuthorization");
+            var model = Assert.IsAssignableFrom<RegistrationViewModel>(viewResult.ViewData.Model);
+            var modelState = viewResult.ViewData.ModelState;
+            Assert.Equal(viewModel, model);
+            Assert.False(modelState.IsValid);
+            Assert.Equal("EnterUserInfo", viewResult.ViewName);
         }
+        [Fact]
+        public void ConfirmMailForRegistration_with_rightMail_returns_errorExistedUser()
+        {
+            // Arrange
+            var mockService = new Mock<IAuthenticateService>();
+            var controller = new AuthenticateController(mockService.Object);
+            var viewModel = new ConfirmMailViewModel
+            {
+                MailAddress = "test@mail.ru"
+            };
+            mockService.Setup(s => s.IsUserExistAsync(viewModel.MailAddress)).ReturnsAsync(() => new());
+
+            // Act
+            var result = controller.ConfirmMailForRegistration(viewModel);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result.Result);
+            var model = Assert.IsAssignableFrom<ConfirmMailViewModel>(viewResult.ViewData.Model);
+            var modelState = viewResult.ViewData.ModelState;
+            Assert.IsType<ConfirmMailViewModel>(model);
+            Assert.False(modelState.IsValid);
+            Assert.Equal("Адрес занят", modelState.Values.First().Errors.First().ErrorMessage);
+        }
+
         #endregion
+
 
         #region Enter user info for registration
         
@@ -226,11 +293,9 @@ namespace Website.Tests
             var result = controller.EnterUserInfo(viewModel);
 
             // Assert
-            var viewResult = Assert.IsType<ViewResult>(result.Result);
-
-            Assert.IsType<ConfirmMailViewModel>(viewResult.Model);
-            Assert.True(viewResult.ViewName=="ConfirmMailForRegistration");
-        }
+            var redirect = Assert.IsType<RedirectToActionResult>(result.Result);
+            
+            Assert.Equal(nameof(controller.ConfirmMailForRegistration),redirect.ActionName);}
 
         #endregion
     }
