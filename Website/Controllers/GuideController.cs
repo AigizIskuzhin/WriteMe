12fb290fd765c4766af3eb5245fe1d713aa20006
Adjax <aigiz.iskuzhin@yandex.ru>
2021-11-12 19:36:46 +0500

    Гайд начало 1

---
+++ Website/Controllers/GuideController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Website.Controllers
+{
+    public class GuideController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Authorization() => View();
+    }
+}
