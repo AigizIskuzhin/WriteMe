d829d47abcd9cbf14b6e8465d72d0fcf062097e3
Adjax <aigiz.iskuzhin@yandex.ru>
2021-11-26 23:29:03 +0500

    Создал начальные расширения для сущностей под преобразование в ViewModel

---
+++ Services/EntitiesConverterExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Database.DAL.Entities;
+using Website.ViewModels;
+using Website.ViewModels.Users;
+
+namespace Services
+{
+    public static class EntitiesConverterExtensions
+    {
+        public static UserViewModel GetViewModel(this User user) => new()
+        {
+            Id = user.Id,
+            Name = user.Name,
+            Surname = user.Surname,
+            Patronymic = user.Patronymic,
+            Birthday = user.Birthday,
+            AvatarPath = user.AvatarPath,
+            Country = user.Country.Name,
+            AccessLevel = user.Role.Code.Equals("user")?AccessLevel.user:user.Role.Code.Equals("mod")?AccessLevel.mod:AccessLevel.admin
+        };
+
+        public static PreviewProfileViewModel GetPreviewViewModel(this User user) => new()
+        {
+            Id = user.Id,
+            Name = user.Name,
+            Surname = user.Surname,
+            Patronymic = user.Patronymic,
+            AvatarPath = user.AvatarPath
+        };
+
+        public static UserPostViewModel GetViewModel(this UserPost p) => new()
+        {
+            Id = p.Id,
+            CreatedDateTime = p.CreatedDateTime,
+            Description = p.Description,
+            Title = p.Title,
+            Owner = GetViewModel(p.Owner)
+        };
+    }
+}
